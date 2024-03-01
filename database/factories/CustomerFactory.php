731522f6b45a94b76f7ea98f3bd1f8c51a2f78e4
iarley <7iarley7@gmail.com>
2024-03-01 00:40:13 -0300

    Feat: Create Factorys for Customer and Invoice

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,8 +16,19 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+        // random type, where could be INDIVIDUAL or BUSINESS
+        $type = $this->faker->randomElement(['I', 'B']);
+        // If type is INDIVIDUAL, create a random name if not create a business name
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'posta_code' => $this->faker->postCode()
         ];
     }
 }
--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,16 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        // create a random state where could be Billed, Paid or void
+        $status = $this->faker->randomElement(['B', 'P', 'V']);
+
         return [
-            //
+            // Relationship between Invoice and Customer id
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'PAID_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL,
         ];
     }
 }
