5761bd12b397b67aa2c2ea5488e243e580e2ed63
iarley <7iarley7@gmail.com>
2024-03-02 18:15:55 -0300

    Fix: Move Customer and Invoice Requests to new folder V1

---
+++ app/Http/Requests/V1/StoreCustomerRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // authorize the user to create a customer
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // applying the validation rules
+        // accepts only types I or B and whatever other types is rejected
+
+        return [
+            'name'       => ['required'],
+            'type'       => ['required', Rule::in(['I', 'B', 'i', 'b'])],
+            'email'      => ['required', 'email'],
+            'address'    => ['required'],
+            'city'       => ['required'],
+            'state'      => ['required'],
+            'postalCode' => ['required'],
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'postal_code' => $this->postalCode,
+        ]);
+    }
+}
--- app/Http/Requests/V1/UpdateInvoiceRequest.php
+++ app/Http/Requests/V1/UpdateInvoiceRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreInvoiceRequest extends FormRequest
+class UpdateInvoiceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +22,7 @@ class StoreInvoiceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            // 
         ];
     }
 }
