56c288224fea92b69d22fc849a3f45f4370702a0
iarley <7iarley7@gmail.com>
2024-03-02 19:35:16 -0300

    Feat: Implement Put and Patch methods to Customer

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -69,7 +69,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
